715f2b13515835e3fd8bcb21c8e5b070dd5dff22
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2016-09-07 17:46:08 -0300

    Prototype package.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='image2tiled',
+    version='0.1.0',
+    packages=['image2tiled'],
+    url='https://github.com/henriquegemignani/image2tiled',
+    license='GPL v3',
+    author='Henrique Gemignani',
+    author_email='henrique@gemignani.org',
+    description='Tool that creates a tiled map based on an image',
+
+    tests_require=[
+        'pytest',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
