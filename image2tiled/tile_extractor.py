7d5860a7e6c26f888be4af197a4c0b83ad6447b7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2016-09-07 19:20:23 -0300

    Add TileExtractor.Results.

--- image2tiled/tile_extractor.py
+++ image2tiled/tile_extractor.py
@@ -1,16 +1,21 @@
+import collections
 
 
 class TileExtractor:
+    Results = collections.namedtuple("Results", ["unique_images", "by_position"])
+
     def extract(self, reader):
         num_cols, num_rows = reader.num_tiles
 
-        images_by_bytes = {}
+        unique_images = collections.OrderedDict()
+        by_position = {}
 
         for row in range(num_rows):
             for col in range(num_cols):
                 image = reader.get_tile(col, row)
                 image_bytes = image.tobytes()
-                if image_bytes not in images_by_bytes:
-                    images_by_bytes[image_bytes] = image
+                if image_bytes not in unique_images:
+                    unique_images[image_bytes] = image
+                by_position[(col, row)] = image_bytes
 
-        return images_by_bytes
+        return TileExtractor.Results(unique_images, by_position)
--- tests/test_tile_extractor.py
+++ tests/test_tile_extractor.py
@@ -9,4 +9,10 @@ def extractor():
 
 
 def test_extract(extractor, reader_2x2):
-    assert len(extractor.extract(reader_2x2)) == 12
+    results = extractor.extract(reader_2x2)
+    assert len(results.unique_images) == 12
+    assert len(results.by_position) == 15
+    assert results.by_position[(2, 0)] == results.by_position[(3, 0)]
+    unique_images = list(results.unique_images)
+    assert unique_images[0] == results.by_position[(0, 0)]
+    assert unique_images[2] == results.by_position[(2, 0)]
