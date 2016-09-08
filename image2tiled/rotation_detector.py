44c777cdc9735d9221a58b115ec756e409aa4c99
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2016-09-07 21:23:03 -0300

    Add RotationDetector

--- image2tiled/rotation_detector.py
+++ image2tiled/rotation_detector.py
@@ -1,10 +1,44 @@
+from PIL import Image
 import collections
 
 
 RotatedTile = collections.namedtuple("RotatedTile", ["image", "rotation"])
 
+
+def _flip(image, method):
+    if method is None:
+        return image
+    else:
+        return image.transpose(method)
+
+
+def _create_flips(image):
+    results = []
+    for horizontal_flip in (None, Image.FLIP_LEFT_RIGHT):
+        h_flip_image = _flip(image, horizontal_flip)
+        for rotation_flip in (None, Image.ROTATE_90, Image.ROTATE_180, Image.ROTATE_270):
+            results.append((_flip(h_flip_image, rotation_flip), (horizontal_flip, rotation_flip)))
+    return results
+
+
+
 class RotationDetector:
-    Results = collections.namedtuple("Results", ["unique_images", "tiles", "by_position"])
+    Results = collections.namedtuple("Results", ["unique_images", "by_position"])
 
     def detect(self, extraction_results):
-        pass
+        unique_images = []
+        by_position = {}
+
+        old_to_new_mapping = {}
+        for image_bytes, image in extraction_results.unique_images.items():
+            if image_bytes not in old_to_new_mapping:
+                # This image is a new one
+                flips = _create_flips(image)
+                unique_images.append(image)
+                for rot_image, rotation in flips:
+                    old_to_new_mapping[rot_image.tobytes()] = RotatedTile(image, rotation)
+
+        for tile, bytes in extraction_results.by_position.items():
+            by_position[tile] = old_to_new_mapping[bytes]
+
+        return RotationDetector.Results(unique_images, by_position)
--- tests/test_rotation_detector.py
+++ tests/test_rotation_detector.py
@@ -3,5 +3,12 @@
 def test_detect(rotation_detector, extract_results_2x2):
     rotation_results = rotation_detector.detect(extract_results_2x2)
     assert len(rotation_results.unique_images) == 7
-    assert len(rotation_results.tiles) == 12
     assert len(rotation_results.by_position) == 15
+    unique_images = list(rotation_results.unique_images)
+    assert unique_images[0].tobytes() == rotation_results.by_position[(0, 0)].image.tobytes()
+    assert unique_images[2].tobytes() == rotation_results.by_position[(2, 0)].image.tobytes()
+
+    print(rotation_results.by_position[(2, 0)])
+    print(rotation_results.by_position[(0, 2)])
+
+    assert rotation_results.by_position[(0, 2)].image.tobytes() == rotation_results.by_position[(2, 0)].image.tobytes()
