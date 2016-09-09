2ebb76b51affb07cb04f590114ae9b477749c952
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2016-09-09 00:18:31 -0300

    Add more tests.

--- tests/test_tiled_generator.py
+++ tests/test_tiled_generator.py
@@ -1,5 +1,9 @@
 import image2tiled.tiled_generator
 import json
+import os
+import pytest
+
+_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_json(image_exporter, tile_extractor, reader_4x4, tiled_4x4):
@@ -14,3 +18,26 @@ def test_json(image_exporter, tile_extractor, reader_4x4, tiled_4x4):
                                                        tiles_per_row=6)
     tiled.add_layer(rotation_results, new_image)
     assert tiled.json() == tiled_4x4_data
+
+
+@pytest.mark.parametrize("filename", [
+"example_4_small1",
+"example_4_small2",
+"example_4_original",
+])
+def test_json_full_16x16(image_exporter, tile_extractor, filename):
+    import image2tiled.image_reader
+    reader = image2tiled.image_reader.ImageReader(os.path.join(_path, "sample_files", filename + ".png"), 16)
+    results = tile_extractor.extract(reader)
+    images_per_row = 128
+
+    new_image = image_exporter.create(results.unique_images, images_per_row)
+    new_image.filename = filename + "-tilemap.png"
+    with open(os.path.join(_path, "sample_files", filename + ".json")) as tiled_file:
+        tiled_data = json.load(tiled_file)
+
+    tiled = image2tiled.tiled_generator.TiledGenerator(reader.tile_size,
+                                                       reader.num_tiles,
+                                                       tiles_per_row=images_per_row)
+    tiled.add_layer(results, new_image)
+    assert tiled.json() == tiled_data
